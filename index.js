6ea7f98347c5275966d77cef77797b29cd854f3d
John Xie <john.xie@sap.com>
2018-07-05 17:35:39 -0700

    cleanup index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var sha1 = require("sha1");
 var crypto = require("crypto");
-var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 80);
 
 app.get('/wx', function(req, res) {
-	var output = 'Once upon a time...';
 	var signature = req.query.signature;
 	var timestamp = req.query.timestamp;
 	var nonce = req.query.nonce;
@@ -20,7 +17,7 @@ app.get('/wx', function(req, res) {
     if (sig === signature) {
 		res.send(echostr);
 	} else {
-		res.json(data);
+		res.send("");
 	}
 });
 
