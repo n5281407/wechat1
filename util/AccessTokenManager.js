4f82b33bd9a5ac2401b310c5e00cad7c047efd10
John Xie <john.xie@sap.com>
2018-07-09 15:14:30 -0700

    continue change

--- util/AccessTokenManager.js
+++ util/AccessTokenManager.js
@@ -1,34 +1,12 @@
 var request = require("request");
 var token = "";
-// var appid = "wx0b0a02c65d81e00a";
-// var secret = "787c322980d3510d39e97281eced12af";
-// var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + secret;
 var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx0b0a02c65d81e00a&secret=787c322980d3510d39e97281eced12af";
-
-// var updateToken = function() {
-//     request.get(postUrl, (err, res, body) => {
-//         if (!err) {
-//             token = JSON.parse(body).access_token;
-// 			console.log("token updated with: " + token);
-//         }
-//     });
-// };
-
-// var getToken = function() {
-//     return token;
-// };
-
-// var start = function() {
-//     setInterval(() => {
-//         updateToken();
-//     }, 5400000);
-// };
 
 var updateToken = function() {
     request.get(postUrl, (err, res, body) => {
-        console.log('err is: ' + err);
-        console.log('res is: ' + res);
-        console.log('body is: ' + body);
+        // console.log('err is: ' + err);
+        // console.log('res is: ' + res);
+        // console.log('body is: ' + body);
         if (!err) {
             token = JSON.parse(body).access_token;
             console.log("token updated with: " + token);
@@ -48,13 +26,4 @@ exports.start = function() {
         updateToken();
     }, 5400000);
 };
-// function getToken() {
-//     return token;
-// }
-
-// function start() {
-//     setInterval(() => {
-
-//     }, 5400000);
-// }
 
