2dec2133a1725b196dbe919cbc6fcc8c69cf1a13
John Xie <john.xie@sap.com>
2018-07-09 15:11:59 -0700

    continue change

--- util/AccessTokenManager.js
+++ util/AccessTokenManager.js
@@ -1,9 +1,9 @@
 var request = require("request");
 var token = "";
-var appid = "wx0b0a02c65d81e00a";
-var secret = "787c322980d3510d39e97281eced12af";
-var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + secret;
-//var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx0b0a02c65d81e00a&secret=787c322980d3510d39e97281eced12af";
+// var appid = "wx0b0a02c65d81e00a";
+// var secret = "787c322980d3510d39e97281eced12af";
+// var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + secret;
+var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx0b0a02c65d81e00a&secret=787c322980d3510d39e97281eced12af";
 
 // var updateToken = function() {
 //     request.get(postUrl, (err, res, body) => {
