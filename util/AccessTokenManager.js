6b55952c2f3e3d5657b59975195fe0aafa052f35
John Xie <john.xie@sap.com>
2018-07-10 10:21:06 -0700

    change token

--- util/AccessTokenManager.js
+++ util/AccessTokenManager.js
@@ -1,23 +1,24 @@
 var request = require("request");
-this.token = "";
+var token = "";
 var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx0b0a02c65d81e00a&secret=787c322980d3510d39e97281eced12af";
 
 var updateToken = function() {
-    var that = this;
     request.get(postUrl, (err, res, body) => {
         // console.log('err is: ' + err);
         // console.log('res is: ' + res);
         // console.log('body is: ' + body);
         if (!err) {
-            that.token = JSON.parse(body).access_token;
-            console.log("token updated with: " + that.token);
+            token = JSON.parse(body).access_token;
+            console.log("token updated with: " + token);
         }
     });
 };
 
+exports.token = token;
+
 exports.getToken = function() {
     console.log("invoking getToken...");
-    return this.token;
+    return token;
 };
 exports.start = function() {
     console.log('invoking start...');
