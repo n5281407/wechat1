0bf3489c1cccdbd7741869bb0bf198d331bee5b2
John Xie <john.xie@sap.com>
2018-07-10 10:17:57 -0700

    empty token issue

--- util/AccessTokenManager.js
+++ util/AccessTokenManager.js
@@ -1,22 +1,23 @@
 var request = require("request");
-var token = "";
+this.token = "";
 var postUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx0b0a02c65d81e00a&secret=787c322980d3510d39e97281eced12af";
 
 var updateToken = function() {
+    var that = this;
     request.get(postUrl, (err, res, body) => {
         // console.log('err is: ' + err);
         // console.log('res is: ' + res);
         // console.log('body is: ' + body);
         if (!err) {
-            token = JSON.parse(body).access_token;
-            console.log("token updated with: " + token);
+            that.token = JSON.parse(body).access_token;
+            console.log("token updated with: " + that.token);
         }
     });
 };
 
 exports.getToken = function() {
     console.log("invoking getToken...");
-    return token;   
+    return this.token;
 };
 exports.start = function() {
     console.log('invoking start...');
