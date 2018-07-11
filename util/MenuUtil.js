09914af784abedfda13d809f73a000e8ecf2facb
John Xie <john.xie@sap.com>
2018-07-10 17:19:29 -0700

    retext

--- util/MenuUtil.js
+++ util/MenuUtil.js
@@ -18,25 +18,26 @@ var menuObj = {
 
 var obj =  {
     "button":[
-    {    
-         "type":"click",
-         "name":"今日歌曲",
-         "key":"V1001_TODAY_MUSIC"
-     },
-     {
-          "name":"菜单",
-          "sub_button":[
-          {    
-              "type":"view",
-              "name":"搜索",
-              "url":"http://www.soso.com/"
-           },
-           {
-              "type":"click",
-              "name":"赞一下我们",
-              "key":"V1001_GOOD"
-           }]
-      }]
+        {    
+            "type":"click",
+            "name":"指令集",
+            "key":"V1001_COMMAND"
+        },
+        {
+            "name":"关于",
+            "sub_button":[
+            {    
+                "type":"view",
+                "name":"搜索",
+                "url":"http://www.soso.com/"
+            },
+            {
+                "type":"click",
+                "name":"关于我们",
+                "key":"V1001_US"
+            }]
+        }
+    ]
 };
 
 var createMenu = function() {
